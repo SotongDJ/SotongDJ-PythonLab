1d55bfb5444834d592c15c74591af78556102c4f
David Soh <sotong0221@gmail.com>
2011-11-28 02:30:00 +0800

    Employ somethings+Incomplete coding

--- Exp1-Rename.py/rename.py
+++ Exp1-Rename.py/rename.py
@@ -1,26 +1,7 @@
 import os
-os.system("dir /B>tmp")
-##Write down the strings you want to change from and change to in the following format:
-##dict={"string_you_want_to_change_from":"string_you_want_to_change_to", ...
-##Example:
-##dict={\
-##"a":"1", \
-##"b":"2", \
-##"c":"3", \
-##"d":"4"}   P.S. you can use "\" to split one line to many seperate lines
-##
-dict={\
-"":"", \
-"":""}
-##
-##After that, write down all the strings you wnat to change from in the follow format:
-##list=["first_string_you_want_to_change_from", "second_string_you_want_to_change_from", ...]
-##Example:
-##list=[\
-##"a", "b", \
-##"c", "d"]
-##
-list=[\
-"", "", \
-"", ""]
-##
+dsln="-------------------------"
+print "Welcome!"
+print dsln
+print "First, tell me the folder which you want to clean up, current?"
+print "If the answer is \"Yes\", key-in .
+print dsln
