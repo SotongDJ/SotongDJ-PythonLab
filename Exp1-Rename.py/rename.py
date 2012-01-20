30688c4d729a325a2cc8a076db891a55d34987e7
David Soh <sotong0221@gmail.com>
2012-01-20 21:22:44 +0800

    start programming first part-locate directory

--- Exp1-Rename.py/rename.py
+++ Exp1-Rename.py/rename.py
@@ -1,7 +1,28 @@
 import os
+## defination start--------------------------------
 dsln="-------------------------"
+key1=""
+blank=""
+line1=""
+loc=""
+tmp=[]
+## defination end----------------------------------
 print "Welcome!"
 print dsln
 print "First, tell me the folder which you want to clean up, current?"
-print "If the answer is \"Yes\", key-in .
+print "If the answer is \"Yes\", press ENTER key."
+print "If not, please key-in the location:"
 print dsln
+key1=raw_input()
+if key1 == blank:
+	os.system("pwd>/tmp/pytmp")
+	tmp=open("/tmp/pytmp").read().splitlines()
+	if len(tmp) == 1:
+		for line1 in tmp:
+			loc=line1
+	else:
+		print "Input Format Error:Please re-open program."
+else:
+	loc=key1
+#os.system("echo "+loc) --debugUsage
+
