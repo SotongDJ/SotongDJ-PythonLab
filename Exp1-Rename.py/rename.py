db55124536ae9f4f507f09b196ad08a021af8511
David Soh <sotong0221@gmail.com>
2012-01-21 13:11:48 +0800

    finish prog. and finish testing

--- Exp1-Rename.py/rename.py
+++ Exp1-Rename.py/rename.py
@@ -1,19 +1,28 @@
 import os
+import sys
 ## defination -- start-----------------------------
 dsln="-------------------------"
 key1=""
 loc=""
-filelist=[]
-global tmps,tmpl,blank,line
+filelist1=[]
+filelist2=[]
+chlist=[]
+global tmps,tmpl1,tmpl2,blank,line1,line2,argv
 tmps=""
-tmpl=[]
+tmpl1=[]
+tmpl2=[]
 blank=""
-line=""
+line1=""
+line2=""
+argv=""
 def clean():
 	tmps=""
-	tmpl=[]
+	tmpl1=[]
+	tmpl2=[]
 	blank=""
-	line=""
+	line1=""
+	line2=""
+	os.system("rm /tmp/pytmp")
 ## defination -- end-------------------------------
 ##
 clean()
@@ -23,14 +32,14 @@ print "Welcome!"
 print dsln
 print "First, tell me the folder which you want to clean up, current?"
 print "If the answer is \"Yes\", press ENTER key."
-print "If not, please key-in the location:"
+print "If not, please key-in the location(absolute address): "
 print dsln
 key1=raw_input()
 if key1 == blank:
 	os.system("pwd>/tmp/pytmp")
-	tmpl=open("/tmp/pytmp").read().splitlines()
-	if len(tmpl) == 1:
-		for line1 in tmpl:
+	tmpl1=open("/tmp/pytmp").read().splitlines()
+	if len(tmpl1) == 1:
+		for line1 in tmpl1:
 			loc=line1
 	else:
 		print "Input Format Error:Please re-open program."
@@ -41,15 +50,42 @@ print "Directory: "+loc
 ##
 clean()
 ## list file -- start-----------------------------------
-os.system("ls "+loc)
-os.system("ls -1 "+loc+">/tmp/pytmp")
-filelist=open("/tmp/pytmp").read().splitlines()
+print "Content files:"
+os.system("ls \""+loc+"\"")
+os.system("ls -1 \""+loc+"\">/tmp/pytmp")
+filelist1=open("/tmp/pytmp").read().splitlines()
 ## list file -- end------------------------------------
 ##
 clean()
-## IRD -- start---------------------------------------
-## des:input replacement dictionary
+## Key-in RD -- start-------------------------------
+## des:key-in replacement dictionary
 print dsln
-print "Now, You can"
+print "Now, You can key-in the words you want to replace"
+print "into gedit. Please follow the format."
+print "  "
+print "Notice:Be carefull don't use special symbol like "
+print "\"?\", \"=\", \"*\" and \"##\", to prevent error."
 print dsln
-## IRD -- end----------------------------------------
+title="## Example:\nchange_from=change_to ##"
+file=open("/tmp/pytmp",'w')
+file.write(title)
+file.close()
+os.system("gedit /tmp/pytmp")
+raw_input("If you finish key-in, press ENTER.")
+chlist=open("/tmp/pytmp").read().splitlines()
+## Key-in RD -- end--------------------------------
+## 
+clean()
+## generate chlist -- start -----------------------
+file=open(loc+"/rename.sh",'w')
+file.write("#/bin/sh\n")
+for line1 in filelist1:
+	tmps="\""+loc+"/"+line1+"\""
+	for line2 in chlist:
+		if not "##" in line2:
+			tmpl1=line2.split("=")
+			line1=line1.replace(tmpl1[0],tmpl1[1])
+	line1="\""+loc+"/"+line1+"\""
+	file.write("mv "+tmps+" "+line1+"\n")
+file.close()
+os.system("cat \""+loc+"/rename.sh\"")
