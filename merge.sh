f70a2ebe8aaad77bb2b1071e1dbdd6855cacdcce
SotongDJ <sotong0221@gmail.com>
2013-06-30 21:39:01 +0800

    fix merge.sh

--- merge.sh
+++ merge.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-python ./gitcho.py --action=checkout --mode=ifnot --target=master
+echo "make sure you are in \"testing\" branch"
+read null
+git checkout master
 git merge testing
 git push origin master
 git checkout testing
